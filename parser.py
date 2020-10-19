9c1062c73728ad66c1db5752db3ee5923dcab6f6
Derek Rodriguez <derek.rodriguez@pm.me>
2020-10-19 17:27:16 -0400

    removed rejected quotes + improved parser code

--- parser.py
+++ parser.py
@@ -1,10 +1,21 @@
 import sys
+from typing import List
+
+def generate_csv(file_list:List[str]):
+    """
+    Aggregates cleaned quotes to as an CSV to stdout.
+
+    Note: This is fragile code optimized around the structure of the w3m renders of
+    bash.org. Also, the list comprehension is very much not performant.
+    """
+    for filename in file_list:
+        with open(filename, "r") as fh:
+            quote_id = "".join([char for char in filename if char.isnumeric()])
+            text = [line.lstrip() for line in fh]
+            header = text[0]
+            score = int(header[header.find("(") + 1 : header.find(")")])
+            body = "".join([line.replace("\n","\\n") for line in text[1:]])
+            print(f"{quote_id},{score},\"{body}\"")
 
 if __name__ == "__main__":
-    for filename in sys.argv[1:]:
-        with open(filename, "r") as fh:
-            for num, line in enumerate(fh):
-                if num == 0:
-                    print(f"{filename}:{line[line.find('(')+1:line.find(')')]}")
-                else:
-                    continue
+    generate_csv(sys.argv[1:])
